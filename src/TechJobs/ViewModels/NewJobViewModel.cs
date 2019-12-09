0411bf2e98ddc1990a61855e2347c3e68d814d3d
Elizabeth Reiher <tonomoshia@gmail.com>
2019-12-09 13:50:15 -0500

    TODO #3 and TODO #4

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -14,16 +14,16 @@ namespace TechJobs.ViewModels
         [Required]
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
-
         // TODO #3 - Included other fields needed to create a job,
         // with correct validation attributes and display names.
+        public int LocationID { get; set; }
 
-        //Use a list of SelectListItems for Employers (while not forgetting the getter and setter) and set that equal to a new list of SelectListItems
+        [Display(Name = "Skill")]
+        public int CoreCompetencyID { get; set; }
 
-        //possible not for sure about this:
-        public Employer type(get; set; ) = new Employer
-        // not sure about above
-        public List<SelectListItem> Employers { get; set; } = new List a n<SelectListItem>();
+        public int PositionTypeID { get; set; }
+
+        public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> CoreCompetencies { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> PositionTypes { get; set; } = new List<SelectListItem>();
@@ -41,13 +41,32 @@ namespace TechJobs.ViewModels
                     Text = field.Value
                 });
             }
-            foreach (var item in collection)
-            {
-                //Use the add method effectively . . . 
-            }
             // TODO #4 - populate the other List<SelectListItem>
             // collections needed in the view
-            //Create a
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
         }
     }
 }
