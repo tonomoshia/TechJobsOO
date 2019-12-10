3d3da752718a02b0ef7cea1cacc1fbc90f8def10
Elizabeth Reiher <tonomoshia@gmail.com>
2019-12-09 20:17:59 -0500

    TODO #7.1 and TODO #7.2

--- src/TechJobs/ViewModels/JobFieldsViewModel.cs
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -14,25 +14,5 @@ namespace TechJobs.ViewModels
 
         // All fields in the given column
         public IEnumerable<JobField> Fields { get; set; }
-
-        // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
-
-        // View title
-        public string Title { get; set; } = "";
-
-        public JobFieldsViewModel()
-        {
-            // Populate the list of all columns
-
-            Columns = new List<JobFieldType>();
-
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-            {
-                Columns.Add(enumVal);
-            }
-
-
-        }
     }
 }
